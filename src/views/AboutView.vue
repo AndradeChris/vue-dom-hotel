c1b2b20c0dc31196d274728b99c37b164d16b838
Flaviano Rodrigues <flavianodev@gmail.com>
2022-11-30 21:33:48 -0300

    added about route

---
+++ src/views/AboutView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <PrincipalTitle text="Página Sobre" />
+  </div>
+</template>
+
+<script>
+
+import PrincipalTitle from '@/components/PrincipalTitle.vue'
+
+export default {
+  name: 'AboutView',
+  components: {
+    PrincipalTitle
+  }
+}
+
+</script>
