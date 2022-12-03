d5615c68dabed3ff50af1c51422d870ee36d50dc
Andrade Christian <andrade.dias.christian@gmail.com>
2022-12-03 18:44:06 -0300

    create stepSub organismm

---
+++ src/components/organisms/StepSubTitle.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="wrapper-step">
+    <MiniTitle :text="step" />
+    <SubTitle :text="subTitle" />
+  </div>
+</template>
+
+<script>
+import SubTitle from '@/components/atoms/SubTitle.vue';
+import MiniTitle from '@/components/atoms/MiniTitle.vue';
+
+export default {
+  name: "StepSubTitle",
+  props: {
+    step: String,
+    subTitle: String,
+    size: String
+  },
+  components: {
+    SubTitle,
+    MiniTitle
+  }
+};
+</script>
+
+<style scoped>
+.wrapper-step {
+  display: flex;
+}
+
+.wrapper-step h2,
+.wrapper-step h4 {
+  font-size: v-bind(size);
+}
+</style>
