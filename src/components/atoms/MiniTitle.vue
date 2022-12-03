ee4ef33856c583b8d3b180afaa7cf193f45ff16c
Andrade Christian <andrade.dias.christian@gmail.com>
2022-12-03 18:43:20 -0300

    create miniTitle atom

---
+++ src/components/atoms/MiniTitle.vue
@@ -0,0 +1,18 @@
+<template>
+  <h4>{{ text }}</h4>
+</template>
+
+<script>
+export default {
+  name: 'MiniTitle',
+  props: {
+    text: String
+  }
+};
+</script>
+
+<style scoped>
+h4 {
+  color: var(--green-dark)
+}
+</style>
