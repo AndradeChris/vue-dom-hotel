79992bfbfdffb066387b395fbbea599f5ca39ef3
Andrade Christian <andrade.dias.christian@gmail.com>
2022-12-03 18:43:06 -0300

    create descriptionParagraph atom

---
+++ src/components/atoms/DescriptionParagraph.vue
@@ -0,0 +1,19 @@
+<template>
+  <p>{{ text }}</p>
+</template>
+
+<script>
+export default {
+  name: 'DescriptionParagraph',
+  props: {
+    text: String
+  }
+};
+</script>
+
+<style scoped>
+p {
+  text-align: justify;
+  line-height: 1.5rem;
+}
+</style>
