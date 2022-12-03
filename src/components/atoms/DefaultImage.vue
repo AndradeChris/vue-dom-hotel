d5927a503fdab3d061b7be0d94cf5ab609dd4951
Andrade Christian <andrade.dias.christian@gmail.com>
2022-12-03 18:42:50 -0300

    create defaultImage atom

---
+++ src/components/atoms/DefaultImage.vue
@@ -0,0 +1,19 @@
+<template>
+  <img :src="src" />
+</template>
+
+<script>
+export default {
+  name: 'DefaultImage',
+  props: {
+    src: String
+  }
+};
+</script>
+
+<style scoped>
+img {
+  width: 100%;
+  border-radius: 10px;
+}
+</style>
